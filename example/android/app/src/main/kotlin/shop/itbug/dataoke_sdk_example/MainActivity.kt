8f1da9eb25ca2ecac8c7b8d3c905cd0969fd39cc
梁典典 <413153189@qq.com>
2022-09-28 14:15:36 +0800

    移除测试包

--- example/android/app/src/main/kotlin/shop/itbug/dataoke_sdk_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package shop.itbug.dataoke_sdk_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- example/android/app/src/main/kotlin/shop/itbug/example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package shop.itbug.example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
